9ecf1a00919f5e072893570563b0d005de350400
BAJNOKDANI <szlobodnik.daniel.2022i@bankitatabanya.hu>
2025-02-06 18:12:15 +0100

    main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+        Konyv k1 = new Konyv("ABA", "Mese1", "Péter", 2008, true);
+        Konyv k2 = new Konyv("BAB", "Mese2", "Zéti", 2007, true);
+
+        Olvaso o1 = new Olvaso("1", "Gabi", "gabi@gmail.com");
+        Olvaso o2 = new Olvaso("2", "Gabi2", "gabi@gmail.com2");
+
+        Kolcsonzo kolcsonzo=new Kolcsonzo();
+
+        kolcsonzo.konyvHozzaad(k1);
+        kolcsonzo.konyvHozzaad(k2);
+
+        kolcsonzo.olvasoHozzaad(o1);
+        kolcsonzo.olvasoHozzaad(o2);
+
+        kolcsonzo.konyvKolcsonoz(k1.getAzonosito(), o1.getAzonosito());
+
+        kolcsonzo.konyvKolcsonoz(k1.getAzonosito(), o2.getAzonosito());
+
 
     }
 }
